1e5faf38569463224a01f716ae527d927d198ab9
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-30 00:12:44 +0800

    ft atoi base

--- fdf.c
+++ fdf.c
@@ -37,6 +37,30 @@ int	count_words(const char *s, char c)
 	return (count);
 }
 
+int ft_atoi_base(const char *str, int base)
+{
+    int     result;
+    char    *ptr;
+
+    result = 0;
+    ptr = (char *)str;
+    if (base == 16 && ft_strncmp(ptr, "0x", 2) == 0)
+        ptr += 2;
+    while (*ptr)
+    {
+        if (*ptr >= '0' && *ptr <= '9')
+            result = result * base + (*ptr - '0');
+        else if (base > 10 && *ptr >= 'a' && *ptr <= 'f')
+            result = result * base + (*ptr - 'a' + 10);
+        else if (base > 10 && *ptr >= 'A' && *ptr <= 'F')
+            result = result * base + (*ptr - 'A' + 10);
+        else
+            break;
+        ptr++;
+    }
+    return (result);
+}
+
 void	parse_map(char *filename, t_map **map)
 {
 	int		fd;
