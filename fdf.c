f4fa7126beb8a456d3c1b554037fb88b946ca924
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-29 22:55:12 +0800

    parse map function preliminary

--- fdf.c
+++ fdf.c
@@ -16,3 +16,30 @@ void	free_split(char **arr)
 	free(arr);
 }
 
+void	parse_map(char *filename, t_map **map)
+{
+	int		fd;
+	char	*line;
+	t_list	*lines;
+	int		i;
+
+	lines = NULL;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		exit_error("Failed to open file");
+	while (get_next_line(fd, &line) > 0)
+		ft_lstadd_back(&lines, ft_lstnew(line));
+	close(fd);
+	*map = malloc(sizeof(t_map));
+	(*map)->height = ft_lstsize(lines);
+	(*map)->width = count_words((char *)lines->content, ' ');
+	(*map)->z_values = malloc(sizeof(int *) * (*map)->height);
+	i = 0;
+	while (lines)
+	{
+		(*map)->z_values[i] = malloc(sizeof(int) * (*map)->width);
+		parse_line((char *)lines->content, (*map)->z_values[i++]);
+		lines = lines->next;
+	}
+}
+
