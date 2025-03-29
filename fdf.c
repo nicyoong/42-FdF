f4cbc4ce6e13e40b576ce49cf5e3352167cb24d2
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-29 23:12:50 +0800

    keyhook and closehook

--- fdf.c
+++ fdf.c
@@ -196,3 +196,22 @@ void	draw_map(t_data *data)
 	}
 }
 
+int	key_hook(int keycode, t_data *data)
+{
+	if (keycode == 65307)
+	{
+		mlx_destroy_image(data->mlx, data->img);
+		mlx_destroy_window(data->mlx, data->win);
+		exit(0);
+	}
+	return (0);
+}
+
+int	close_hook(t_data *data)
+{
+	mlx_destroy_image(data->mlx, data->img);
+	mlx_destroy_window(data->mlx, data->win);
+	exit(0);
+	return (0);
+}
+
