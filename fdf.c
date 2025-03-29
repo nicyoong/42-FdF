c6c4bf92a812fe3f21e4e0f582b24f045bef9130
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-30 01:32:28 +0800

    separate main function to main.c

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 01:28:14 by nyoong            #+#    #+#             */
-/*   Updated: 2025/03/30 01:30:30 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/03/30 01:32:12 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -298,25 +298,3 @@ int	close_hook(t_data *data)
 	return (0);
 }
 
-int	main(int argc, char **argv)
-{
-	t_data	data;
-
-	if (argc != 2)
-		exit_error("Usage: ./fdf <map.fdf>");
-	parse_map(argv[1], &data.map);
-	data.mlx = mlx_init();
-	data.win_width = 1500;
-	data.win_height = 1500;
-	data.win = mlx_new_window(data.mlx, data.win_width, data.win_height, "FDF");
-	data.img = mlx_new_image(data.mlx, data.win_width, data.win_height);
-	data.img_addr = mlx_get_data_addr(data.img, &data.bpp, &data.line_length, &data.endian);
-	compute_projection(&data);
-	scale_and_center(&data);
-	draw_map(&data);
-	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
-	mlx_hook(data.win, 2, 1L << 0, key_hook, &data);
-	mlx_hook(data.win, 17, 0, close_hook, &data);
-	mlx_loop(data.mlx);
-	return (0);
-}
