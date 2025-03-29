69fc7cf51edb7ca96a2fca67601f795e622f5060
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-30 01:53:40 +0800

    break scale function in fdf.c

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 01:28:14 by nyoong            #+#    #+#             */
-/*   Updated: 2025/03/30 01:43:53 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/03/30 01:53:07 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,46 +28,77 @@ void	compute_projection(t_data *data)
 		while (++j < data->map->width)
 		{
 			data->screen_coords[i][j].x = (j - i) * cos(theta);
-			data->screen_coords[i][j].y = (j + i) * sin(theta) - data->map->points[i][j].z;
+			data->screen_coords[i][j].y = (j + i) * sin(theta)
+				- data->map->points[i][j].z;
 		}
 	}
 }
 
-void	scale_and_center(t_data *data)
+static t_bounds find_bounds(t_data *data)
+{
+	t_bounds bounds = {
+		.x_min = INFINITY,
+		.x_max = -INFINITY,
+		.y_min = INFINITY,
+		.y_max = -INFINITY
+	};
+	int i = -1;
+
+	while (++i < data->map->height)
+	{
+		int j = -1;
+		while (++j < data->map->width)
+		{
+			bounds.x_min = fmin(bounds.x_min, data->screen_coords[i][j].x);
+			bounds.x_max = fmax(bounds.x_max, data->screen_coords[i][j].x);
+			bounds.y_min = fmin(bounds.y_min, data->screen_coords[i][j].y);
+			bounds.y_max = fmax(bounds.y_max, data->screen_coords[i][j].y);
+		}
+	}
+	return (bounds);
+}
+
+static float get_scale_factor(t_data *data, t_bounds bounds)
+{
+	const float x_span = bounds.x_max - bounds.x_min;
+	const float y_span = bounds.y_max - bounds.y_min;
+	const float x_scale = (data->win_width - 40) / x_span;
+	const float y_scale = (data->win_height - 40) / y_span;
+
+	return (fmin(x_scale, y_scale));
+}
+
+static void apply_scaling(t_data *data, float scale, t_bounds bounds)
 {
 	int		i;
 	int		j;
-	float	min_max[4];
-	float	scale;
+	float	x_center;
+	float	y_center;
 
-	min_max[0] = INFINITY;
-	min_max[1] = -INFINITY;
-	min_max[2] = INFINITY;
-	min_max[3] = -INFINITY;
 	i = -1;
+	x_center = (bounds.x_min + bounds.x_max) / 2;
+	y_center = (bounds.y_min + bounds.y_max) / 2;
 	while (++i < data->map->height)
 	{
 		j = -1;
 		while (++j < data->map->width)
 		{
-			min_max[0] = fmin(min_max[0], data->screen_coords[i][j].x);
-			min_max[1] = fmax(min_max[1], data->screen_coords[i][j].x);
-			min_max[2] = fmin(min_max[2], data->screen_coords[i][j].y);
-			min_max[3] = fmax(min_max[3], data->screen_coords[i][j].y);
+			data->screen_coords[i][j].x = (data->screen_coords[i][j].x - x_center) 
+				* scale + data->win_width / 2;
+			data->screen_coords[i][j].y = (data->screen_coords[i][j].y - y_center) 
+				* scale + data->win_height / 2;
 		}
 	}
-	scale = fmin((data->win_width - 40) / (min_max[1] - min_max[0]),
-		(data->win_height - 40) / (min_max[3] - min_max[2]));
-	i = -1;
-	while (++i < data->map->height)
-	{
-		j = -1;
-		while (++j < data->map->width)
-		{
-			data->screen_coords[i][j].x = (data->screen_coords[i][j].x - (min_max[0] + min_max[1]) / 2) * scale + data->win_width / 2;
-			data->screen_coords[i][j].y = (data->screen_coords[i][j].y - (min_max[2] + min_max[3]) / 2) * scale + data->win_height / 2;
-		}
-	}
+}
+
+void scale_and_center(t_data *data)
+{
+	t_bounds	bounds;
+	float		scale;
+
+	bounds = find_bounds(data);
+	scale = get_scale_factor(data, bounds);
+	apply_scaling(data, scale, bounds);
 }
 
 void	put_pixel(t_data *data, int x, int y, int color)
@@ -80,4 +111,3 @@ void	put_pixel(t_data *data, int x, int y, int color)
 		*(unsigned int *)dst = color;
 	}
 }
-
