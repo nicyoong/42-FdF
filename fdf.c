101aaf2b71904b2ddb530978e8aa021a2096d87e
Nicholas Yoong <nicyoong321@gmail.com>
2025-03-29 23:32:03 +0800

    window height and width change

--- fdf.c
+++ fdf.c
@@ -255,8 +255,8 @@ int	main(int argc, char **argv)
 		exit_error("Usage: ./fdf <map.fdf>");
 	parse_map(argv[1], &data.map);
 	data.mlx = mlx_init();
-	data.win_width = 800;
-	data.win_height = 600;
+	data.win_width = 1500;
+	data.win_height = 1500;
 	data.win = mlx_new_window(data.mlx, data.win_width, data.win_height, "FDF");
 	data.img = mlx_new_image(data.mlx, data.win_width, data.win_height);
 	data.img_addr = mlx_get_data_addr(data.img, &data.bpp, &data.line_length, &data.endian);
