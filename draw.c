e5b43faa7c8c29c9e32dda03c4ea013809bebcb8
nicyoong <nicyoong321@gmail.com>
2025-03-30 09:48:40 +0800

    initialize bresenham function in draw.c

--- draw.c
+++ draw.c
@@ -12,50 +12,71 @@
 
 #include "fdf.h"
 
-void	draw_line(t_point p0, t_point p1, t_data *data, int color)
-{
-	t_point	delta;
-	t_point	sign;
-	int		error;
-	int		e2;
-	int		x0;
-	int		y0;
-	int		x1;
-	int		y1;
+t_bresenham init_bresenham(t_point p0, t_point p1) {
+	t_bresenham	b;
 
-	x0 = roundf(p0.x);
-	y0 = roundf(p0.y);
-	x1 = roundf(p1.x);
-	y1 = roundf(p1.y);
-	delta.x = abs(x1 - x0);
-	delta.y = -abs(y1 - y0);
-	if (x0 < x1)
-		sign.x = 1;
+	b.x0 = roundf(p0.x);
+	b.y0 = roundf(p0.y);
+	b.x1 = roundf(p1.x);
+	b.y1 = roundf(p1.y);
+	b.delta.x = abs(b.x1 - b.x0);
+	b.delta.y = -abs(b.y1 - b.y0);
+	if (b.x0 < b.x1)
+		b.sign.x = 1;
 	else
-		sign.x = -1;
-	if (y0 < y1)
-		sign.y = 1;
+		b.sign.x = -1;
+	if (b.y0 < b.y1)
+		b.sign.y = 1;
 	else
-		sign.y = -1;
-	error = delta.x + delta.y;
-	while (1)
-	{
-		put_pixel(data, x0, y0, color);
-		if (x0 == x1 && y0 == y1)
-			break ;
-		e2 = 2 * error;
-		if (e2 >= delta.y)
-		{
-			error += delta.y;
-			x0 += sign.x;
-		}
-		if (e2 <= delta.x)
-		{
-			error += delta.x;
-			y0 += sign.y;
-		}
-	}
+		b.sign.y = -1;
+	b.error = b.delta.x + b.delta.y;
+	return (b);
 }
+
+// void	draw_line(t_point p0, t_point p1, t_data *data, int color)
+// {
+// 	t_point	delta;
+// 	t_point	sign;
+// 	int		error;
+// 	int		e2;
+// 	int		x0;
+// 	int		y0;
+// 	int		x1;
+// 	int		y1;
+
+// 	x0 = roundf(p0.x);
+// 	y0 = roundf(p0.y);
+// 	x1 = roundf(p1.x);
+// 	y1 = roundf(p1.y);
+// 	delta.x = abs(x1 - x0);
+// 	delta.y = -abs(y1 - y0);
+// 	if (x0 < x1)
+// 		sign.x = 1;
+// 	else
+// 		sign.x = -1;
+// 	if (y0 < y1)
+// 		sign.y = 1;
+// 	else
+// 		sign.y = -1;
+// 	error = delta.x + delta.y;
+// 	while (1)
+// 	{
+// 		put_pixel(data, x0, y0, color);
+// 		if (x0 == x1 && y0 == y1)
+// 			break ;
+// 		e2 = 2 * error;
+// 		if (e2 >= delta.y)
+// 		{
+// 			error += delta.y;
+// 			x0 += sign.x;
+// 		}
+// 		if (e2 <= delta.x)
+// 		{
+// 			error += delta.x;
+// 			y0 += sign.y;
+// 		}
+// 	}
+// }
 
 void	draw_map(t_data *data)
 {
