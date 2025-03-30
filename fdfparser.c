0199d3a6cdeeded42781508a21147fe23dd0bebb
nicyoong <nicyoong321@gmail.com>
2025-03-30 10:05:07 +0800

    Norm check fdfparser.c

--- fdfparser.c
+++ fdfparser.c
@@ -12,18 +12,21 @@
 
 #include "fdf.h"
 
-static t_list *read_map_lines(char *filename) {
-    int fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        exit_error("Failed to open file");
+t_list	*read_map_lines(char *filename)
+{
+	int		fd;
+	t_list	*lines;
+	char	*line;
 
-    t_list *lines = NULL;
-    char *line;
-
-    while (1) {
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		exit_error("Failed to open file");
+	lines = NULL;
+	while (1)
+	{
 		line = get_next_line(fd);
-		if (!line) break;
-	
+		if (!line)
+			break ;
 		t_list *node = ft_lstnew(line);
 		if (!node) {
 			free(line);
@@ -31,47 +34,53 @@ static t_list *read_map_lines(char *filename) {
 		}
 		ft_lstadd_back(&lines, node);
 	}
-    close(fd);
-
-    if (!lines)
-        exit_error("Empty map file");
-    return lines;
+	close(fd);
+	if (!lines)
+		exit_error("Empty map file");
+	return (lines);
 }
 
-static t_map *init_map(t_list *lines) {
-    t_map *map = malloc(sizeof(t_map));
-    if (!map)
-        exit_error("Failed to allocate map");
+static t_map *init_map(t_list *lines)
+{
+	t_map	*map;
 
-    map->height = ft_lstsize(lines);
-    map->width = count_words((char *)lines->content, ' ');
-    map->points = malloc(sizeof(t_color *) * map->height);
-    if (!map->points)
-        exit_error("Failed to allocate map points");
-
-    return map;
+	map = malloc(sizeof(t_map));
+	if (!map)
+		exit_error("Failed to allocate map");
+	map->height = ft_lstsize(lines);
+	map->width = count_words((char *)lines->content, ' ');
+	map->points = malloc(sizeof(t_color *) * map->height);
+	if (!map->points)
+		exit_error("Failed to allocate map points");
+	return (map);
 }
 
-static void parse_and_store_lines(t_map *map, t_list *lines) {
-    t_list *current = lines;
-    int i = 0;
+static void parse_and_store_lines(t_map *map, t_list *lines)
+{
+	t_list	*current;
+	int		i;
 
-    while (current) {
-        map->points[i] = malloc(sizeof(t_color) * map->width);
-        if (!map->points[i])
-            exit_error("Failed to allocate row");
-
-        parse_line((char *)current->content, map->points[i++]);
-        free(current->content);
-        current = current->next;
-    }
+	i = 0;
+	current = lines;
+	while (current)
+	{
+		map->points[i] = malloc(sizeof(t_color) * map->width);
+		if (!map->points[i])
+			exit_error("Failed to allocate row");
+		parse_line((char *)current->content, map->points[i++]);
+		free(current->content);
+		current = current->next;
+	}
 }
 
-void parse_map(char *filename, t_map **map) {
-    t_list *lines = read_map_lines(filename);
-    *map = init_map(lines);
-    parse_and_store_lines(*map, lines);
-    ft_lstclear(&lines, NULL);
+void parse_map(char *filename, t_map **map)
+{
+	t_list *lines;
+
+	lines = read_map_lines(filename);
+	*map = init_map(lines);
+	parse_and_store_lines(*map, lines);
+	ft_lstclear(&lines, NULL);
 }
 
 void	parse_line(char *line, t_color *points_row)
